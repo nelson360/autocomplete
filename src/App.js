f97e1d1301f78b875549be297bfd58b2b8b13099
nelson360 <nelsonomondi99@gmail.com>
2021-12-10 13:53:19 +0300

    update to library #1
    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import './styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class Autocomplete extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSuggestion: 0,
+      filteredSuggestions: [],
+      showSuggestions: false,
+      userInput: ""
+    };
+  }
+ 
+onChange = e => {
+  const { suggestions } = this.props;
+  const userInput = e.currentTarget.value;
+
+  const filteredSuggestions = suggestions.filter(
+    suggestion =>
+      suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
   );
-}
 
-export default App;
+  this.setState({
+    activeSuggestion: 0,
+    filteredSuggestions,
+    showSuggestions: true,
+    userInput: e.currentTarget.value
+  });
+
+ onClick = e => {
+  this.setState({
+    activeSuggestion: 0,
+    filteredSuggestions: [],
+    showSuggestions: false,
+    userInput: e.currentTarget.innerText
+  });
+};
+
+onKeyDown = e => {
+  const { activeSuggestion, filteredSuggestions } = this.state;
+
+  if (e.keyCode === 13) {
+    this.setState({
+      activeSuggestion: 0,
+      showSuggestions: false,
+      userInput: filteredSuggestions[activeSuggestion]
+    });
+  } else if (e.keyCode === 38) {
+    if (activeSuggestion === 0) {
+      return;
+    }
+    this.setState({ activeSuggestion: activeSuggestion - 1 });
+  }
+  // User pressed the down arrow, increment the index
+  else if (e.keyCode === 40) {
+    if (activeSuggestion - 1 === filteredSuggestions.length) {
+      return;
+    }
+    this.setState({ activeSuggestion: activeSuggestion + 1 });
+  }
+};
+ 
+render() {
+  const {
+    onChange,
+    onClick,
+    onKeyDown,
+    state: {
+      activeSuggestion,
+      filteredSuggestions,
+      showSuggestions,
+      userInput
+    }
+  } = this;
+
+  let suggestionsListComponent;
+ 
+if (showSuggestions && userInput) {
+  if (filteredSuggestions.length) {
+    suggestionsListComponent = (
+      <ul class="suggestions">
+        {filteredSuggestions.map((suggestion, index) => {
+          let className;
+
+          // Flag the active suggestion with a class
+          if (index === activeSuggestion) {
+            className = "suggestion-active";
+          }
+          return (
+                  <Fragment>
+        <input
+          type="text"
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={userInput}
+        />
+        {suggestionsListComponent}
+      </Fragment>
+            <li className={className} key={suggestion} onClick={onClick}>
+              {suggestion}
+            </li>
+          );
+        })}
+      </ul>
+    );
+  } else {
+    suggestionsListComponent = (
+      <div class="no-suggestions">
+        <em>No suggestions available.</em>
+      </div>
+    );
+  }
+}
+   
+  
+
+
